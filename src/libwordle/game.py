d0696ba96588ef78cbd66d5a3b86f916d8e3252d
Alethea Power <10975066+aletheap@users.noreply.github.com>
2023-09-04 03:23:43 -0700

    a little more tidying

--- src/libwordle/game.py
+++ src/libwordle/game.py
@@ -3,7 +3,6 @@
 
 import datetime
 import string
-from collections import defaultdict
 
 import colors
 import numpy as np
@@ -25,7 +24,6 @@ class WordleGame:
                 "valid_words": valid_words,
                 "word_freqs": word_freqs,
             }
-        self.solutions = word_data["solutions"]
         self.valid_words = word_data["valid_words"]
 
         # record solution for this game
@@ -40,21 +38,28 @@ class WordleGame:
                 wordle_number = (datetime.date.today() - WORDLE_START_DATE).days
             self.wordle_number = wordle_number
             self.wordle_date = WORDLE_START_DATE + datetime.timedelta(days=wordle_number)
-            self.solution = self.solutions[self.wordle_number]
+            self.solution = word_data["solutions"][self.wordle_number]
 
         self.won = False
         self.is_finished = False
         self.all_guesses = []
-        self.all_hints = []
         self.max_guesses = max_guesses
 
     def guess(self, word):
-        word = word.strip().lower()
-
         assert not self.is_finished, "Game is over"
+
+        word = word.strip().lower()
+        assert len(word) == WORD_LENGTH, f'"{word}" is not {WORD_LENGTH} letters long'
         assert word not in self.all_guesses, f"Already guessed word: {word}"
-        assert len(word) == WORD_LENGTH, f"Invalid word length: {word}"
         assert word in self.valid_words, f"Invalid word: {word}"
+
+        self.all_guesses.append(word)
+
+        if word == self.solution:
+            self.won = True
+
+        if self.won or (len(self.all_guesses) == self.max_guesses):
+            self.is_finished = True
 
         hints = [" "] * WORD_LENGTH
         for i, c in enumerate(word):
@@ -64,15 +69,7 @@ class WordleGame:
                 hints[i] = "Y"
             else:
                 hints[i] = "B"
-
-        self.all_hints.append(hints)
-        self.all_guesses.append(word)
-
-        if word == self.solution:
-            self.won = True
-
-        if self.won or (len(self.all_guesses) == self.max_guesses):
-            self.is_finished = True
+        hints = "".join(hints)
 
         return hints
 
@@ -92,17 +89,17 @@ class InteractiveWordleGame:
         self.show_start_header()
 
         while not self.game.is_finished:
+            self.show_all_hints()
+
             prompt = f"Guess {len(self.all_guesses) + 1}: "
             guess = input(prompt).strip().lower()
 
             try:
-                hints = self.game.guess(guess)
+                hints = self.game.guess(guess)  # raises AssertionError if invalid
                 self.all_guesses.append(guess)
                 self.all_hints.append(hints)
             except AssertionError as e:
                 print(e)
-
-            self.show_all_hints()
 
         self.show_game_end()
         return self.game.won
@@ -139,12 +136,12 @@ class InteractiveWordleGame:
 
         # render any existing hints
         for guess, hints in zip(self.all_guesses, self.all_hints):
-            line = [self.render_character(c, h) for h, c in zip(hints, guess)]
+            line = [self.color_character(c, h) for h, c in zip(hints, guess)]
             lines.append(" ".join(line))
 
         # add any remaining blank lines
         for _ in range(self.game.max_guesses - len(self.all_guesses)):
-            line = [self.render_character(" ", "W") for _ in range(WORD_LENGTH)]
+            line = [self.color_character(" ", "W") for _ in range(WORD_LENGTH)]
             lines.append(" ".join(line))
 
         return lines
@@ -160,15 +157,11 @@ class InteractiveWordleGame:
         # render the keyboard rows
         rows = []
         for letters in ("qwertyuiop", "asdfghjkl", "zxcvbnm"):
-            row = [self.render_character(L, kbd_colors[L]) for L in letters]
+            row = [self.color_character(L, kbd_colors[L]) for L in letters]
             rows.append("".join(row))
         rows[-1] = "   " + rows[-1]
 
         return "\n".join(rows)
-
-    def render_character(self, c, hint):
-        hint_colors = {"G": "green", "Y": "yellow", "B": "grey", "W": "white"}
-        return colors.color(f" {c.upper()} ", "black", hint_colors[hint])
 
     def render_shareable_grid(self):
         emojis = {"G": "🟩", "Y": "🟨", "B": "⬛"}
@@ -186,3 +179,7 @@ class InteractiveWordleGame:
         result += "\n".join(color_lines)
 
         return result
+
+    def color_character(self, c, hint):
+        hint_colors = {"G": "green", "Y": "yellow", "B": "grey", "W": "white"}
+        return colors.color(f" {c.upper()} ", "black", hint_colors[hint])
