be93add05b4142ad85f269db6c83866c9ad91d66
Alethea Power <10975066+aletheap@users.noreply.github.com>
2023-09-04 01:49:43 -0700

    add google unigram freqs, reorg data code

--- src/libwordle/data.py
+++ src/libwordle/data.py
@@ -1,50 +1,54 @@
 import json
 import os
+import datetime
 
 MY_DIR = os.path.dirname(os.path.realpath(__file__))
+# Source: "https://bert.org/assets/posts/wordle/words.json"
+WORDLE_DATA_FILE = os.path.join(MY_DIR, "wordle_words.json")
+# Source: https://www.kaggle.com/datasets/rtatman/english-word-frequency
+WORD_FREQS_DATA_FILE = os.path.join(MY_DIR, "word_freqs_google.json")
+WORDLE_START_DATE = datetime.date(2021, 6, 19)
+WORD_LENGTH = 5
+
+CACHE = {
+    "wordle_solutions": [],
+    "wordle_valid_words": [],
+    "word_freqs": {},
+}
 
 
-def load_data(max_vocab_size=None, word_freqs_data_file=None, wordle_data_file=None):
-    wordle_solutions, wordle_valid_words = load_wordle_data(data_file=wordle_data_file)
-    word_freqs = load_word_freqs(
-        max_vocab_size=max_vocab_size,
-        data_file=word_freqs_data_file,
-        only_include_words=wordle_valid_words,
-    )
-
+def load_data():
+    wordle_solutions, wordle_valid_words = load_wordle_data()
+    word_freqs = load_word_freqs()
     return wordle_solutions, wordle_valid_words, word_freqs
 
 
-def load_wordle_data(data_file=None):
-    if data_file is None:
-        data_file = os.path.join(MY_DIR, "wordle_words.json")
-
-    # Load all Wordle words. Source: "https://bert.org/assets/posts/wordle/words.json"
-    with open(data_file, "r") as f:
-        data = json.load(f)
-    solutions = data["solutions"]
-    other_valid_words = set(data["other_valid_words"])
-    all_words = set(solutions) | other_valid_words
-
-    return solutions, all_words
+def load_wordle_data():
+    if not CACHE["wordle_solutions"]:
+        with open(WORDLE_DATA_FILE, "r") as f:
+            data = json.load(f)
+        CACHE["wordle_solutions"] = data["solutions"]
+        CACHE["wordle_valid_words"] = sorted(
+            list(set(data["solutions"]) | set(data["other_valid_words"]))
+        )
+    return CACHE["wordle_solutions"], CACHE["wordle_valid_words"]
 
 
-def load_word_freqs(max_vocab_size=-1, data_file=None, only_include_words=None):
-    if data_file is None:
-        data_file = os.path.join(MY_DIR, "word_freqs.json")
+def load_word_freqs():
+    if not CACHE["word_freqs"]:
+        with open(WORD_FREQS_DATA_FILE, "r") as f:
+            word_freqs = json.load(f)
+        word_freqs = dict(sorted(word_freqs.items(), key=lambda x: x[1], reverse=True))
+        CACHE["word_freqs"] = norm(word_freqs)
+    return CACHE["word_freqs"]
 
-    with open(data_file, "r") as f:
-        word_freqs = json.load(f)
 
-    # only include words
-    if only_include_words is not None:
-        word_freqs = {k: v for k, v in word_freqs.items() if k in only_include_words}
-
-    # truncate to max len
-    word_freqs = dict(sorted(word_freqs.items(), key=lambda x: x[1], reverse=True)[:max_vocab_size])
-
-    # normalize word_freqs
-    total_freqs = sum(word_freqs.values())
-    word_freqs = {w: f / total_freqs for w, f in word_freqs.items()}
-
-    return word_freqs
+def norm(o):
+    if isinstance(o, dict):
+        total = sum(o.values())
+        return {k: v / total for k, v in o.items()}
+    elif isinstance(o, list):
+        total = sum(o)
+        return [x / total for x in o]
+    else:
+        raise ValueError(f"Can't normalize {type(o)}")
